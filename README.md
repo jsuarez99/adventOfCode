17ca393f719f0e7959347fa3d66045b2a2e02567
Joshua Suarez <joshuasuarez@gmail.com>
2015-12-06 17:34:36 -0500

    Day 6 solution added.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # adventOfCode
 
 These are my solutions to the adventofcode.com puzzles. Each day is stored in its own directory with the day's puzzle and input files.
+
+My code of choice for this is Perl.
---
+++ day6/day6b.pl
@@ -0,0 +1,44 @@
+use strict;
+
+#this is a horribly inefficient solution, forgive me....
+
+my $filename = "input";
+open(my $fileh, $filename) or die "Could not find the file '$filename' $!";
+
+my @lightArray; #The array of all our lights.
+
+while(my $row = <$fileh>){
+    chomp $row;
+	
+	#grab all the necessary parts of the line and store them in an array
+	$row =~ m/^(turn off|turn on|toggle) (\d+),(\d+) through (\d+),(\d+)$/;
+    my @cur = ($1,$2,$3,$4,$5);
+
+	#go through our massive array and change the values
+	for(my $x = $cur[1]; $x <= $cur[3]; $x++){
+		for(my $y = $cur[2]; $y <= $cur[4]; $y++){
+			if($lightArray[$y][$x] eq ''){
+				$lightArray[$y][$x] = 0;
+			}
+			
+			if($cur[0] eq "turn on"){
+				$lightArray[$y][$x] += 1;
+			}
+			elsif($cur[0] eq "turn off"){
+				$lightArray[$y][$x] = $lightArray[$y][$x] == 0 ? 0 : $lightArray[$y][$x] - 1;
+			}
+			elsif($cur[0] eq "toggle"){
+				$lightArray[$y][$x] += 2;
+			}
+		}
+	}
+}
+
+my $brightness = 0;
+for(my $x = 0; $x < 1000; $x++){
+	for(my $y = 0; $y < 1000; $y++){
+		$brightness += $lightArray[$y][$x];
+	}
+}
+
+print "Total Brightness: $brightness\n";
