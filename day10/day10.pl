ccad52051a7e578c52d432cfb0043922631349d8
Josh Suarez <jsuarez@varickmm.com>
2015-12-11 11:24:56 -0500

    Day 10 solution added
    To get the solution for part B, change the counter in the for loop
    from 40 to 50.

---
+++ day10/day10.pl
@@ -0,0 +1,31 @@
+use strict;
+
+my $input = "1321131112";
+
+for(my $k = 0; $k < 40; $k++){
+    $input = &lookAndSay($input);
+}
+
+print "Result: ".split(//,$input)."\n";
+
+sub lookAndSay(){
+    my @digits = split(//,$_[0]);
+    my @output;
+    for(my $i = 0; $i < @digits;){
+	my $numTimes = 1;  
+	my $j = $i+1;      #counter to look ahead for the same digit
+	
+	#Keep counting the number of occurances of the current digit
+	while($digits[$i] == $digits[$j]){
+	    $numTimes++;
+	    $j++;
+	}
+
+	#Push the number of occurances and the digit itself to the output
+	push(@output,($numTimes,$digits[$i]));
+
+	#Set the $i counter to the next different digit in the string
+	$i = $j;
+    }
+    return join('',@output);
+}
